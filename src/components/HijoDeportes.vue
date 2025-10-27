715cec78c9d9713833333e90024eda9ba2ea2dbb
DPG210 <diegogrego210@gmail.com>
2025-10-27 14:15:34 +0100

    first commit

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2 style="color:green">{{nombresport}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HijoDeportes",
+    props:["nombresport"],
+    mounted(){
+        console.log(this.nombresport)
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1 style="color:blue">Padre Deportes</h1>
+        <ul>
+            <li v-for="deporte in deportes" :key="deporte">
+                <HijoDeportes :nombresport="deporte"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import HijoDeportes from './HijoDeportes.vue'
+
+export default {
+    name:"PadreDeportes",
+    components:{
+        HijoDeportes
+    },
+    data(){
+        return{
+            deportes:["futbol","baloncesto","padel","tenis"]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
