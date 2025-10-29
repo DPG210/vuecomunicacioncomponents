5d80a7e1562069707e64d8dc3e00ff025d0b2365
DPG210 <diegogrego210@gmail.com>
2025-10-29 20:32:24 +0100

    cambios suma

--- src/components/SumaCheckbox.vue
+++ src/components/SumaCheckbox.vue
@@ -4,7 +4,8 @@
         <button @click="generarNumeros">Generar Numeros</button>
         <div v-for="(num,index) in numeros" :key="index">
             <label>{{ num }}
-                <input type="checkbox" :key="num" :value="num" v-model="numeroSeleccionado"/>
+                <input type="checkbox" :key="num" :value="num" v-model="numeroSeleccionado" 
+                @change="actualizarSuma($event, num)"/>
             </label>
         </div>
         <h2>{{ suma }}</h2>
@@ -24,6 +25,7 @@
         },
         methods:{
             generarNumeros(){
+                this.numeros=[]
                 var numero=0;
                 var i=0
                 for (i;i<6;i++){
@@ -32,12 +34,19 @@
                     console.log(numero)
                     console.log(this.numeros)
                 }
+                this.numeroSeleccionado=[];
+                this.suma=0;
                 
             },
-            sumaNumeros(){
-                var num=this.numeroSeleccionado[0];
-                this.suma+=this.numeroSeleccionado;
+           actualizarSuma(event, num) {
+            // si el checkbox se marca → se suma
+            // si se desmarca → se resta
+            if (event.target.checked) {
+                this.suma += num;
+            } else {
+                this.suma -= num;
             }
+            },
         }
     }
 </script>
